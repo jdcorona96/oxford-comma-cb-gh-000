e5fa2ce710478b10a09f469ff564449fa789ba09
Joseph Corona <josed.hc96@gmail.com>
2018-11-14 05:08:29 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   last = array.pop
   ret = array.join(", ")
   if (array.length != 0) then
-    ret += "and #{last}"
+    ret += ", and #{last}"
   else
     ret += last
   end
